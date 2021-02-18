b964ed3860a294eb7269c433eaec82d1b02ff14c
MukhtarA <33712120+MukhtarA@users.noreply.github.com>
2021-02-18 19:36:59 +0600

    feat: add Boarding

---
+++ src/components/Auth/Boarding/Boarding.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {View} from "react-native";
+
+const Boarding = () => {
+    return(
+        <View style={{ flex: 1, backgroundColor: 'cyan'}} />
+    )
+}
+
+export default Boarding;
